fe9e34295289b72e647da00524837f5cbd0a1857
Jhonatan Fulguera <jfulguera@CREMONA.local>
2024-05-16 12:02:50 -0300

    1 Configuracion de grafico y Plugin de Chart js

--- src/app/controlpanel/[id]/page.jsx
+++ src/app/controlpanel/[id]/page.jsx
@@ -2,36 +2,35 @@
 //Componentes 
 import NavEquipos from "@/nav/NavEquipos";
 import Titulo from "@/lib/Titulo";
-
-//Chart JS
-
-import {Line} from 'react-chartjs-2';
-import {
-    Chart as ChartJS,
-    LineElement,
-    CategoryScale, // x axis
-    LinearScale, // y axis
-    PointElement,
-    Legend,
-    Tooltip,
-    Filler,
-  } from "chart.js";
-  
-  ChartJS.register(
-    LineElement,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    Legend,
-    Tooltip,
-    Filler
-  );
-  
-
-//Dependencas 
 import Link from "next/link";
 
+//Chart JS
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  scales,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import zoomPlugin, { pan, zoom } from 'chartjs-plugin-zoom';
+import { faker } from "@faker-js/faker";
 
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  zoomPlugin
+);
+/*
   const salesData = [
     { month: "January", sales: 100 },
     { month: "February", sales: 150 },
@@ -40,82 +39,66 @@ import Link from "next/link";
     { month: "May", sales: 180 },
     { month: "June", sales: 250 },
   ];
+  */
 
-
-export default function Maquina({params}) {
-    
-  const data = {
-    labels: salesData.map((data) => data.month),
-    datasets: [
-      {
-        label: "Revenue",
-        data: salesData.map((data) => data.sales),
-        borderColor: "#cb0c9f",
-        borderWidth: 3,
-        pointBorderColor: "#cb0c9f",
-        pointBorderWidth: 3,
-        tension: 0.5,
-        fill: true,
-        backgroundColor: (context) => {
-          const ctx = context.chart.ctx;
-          const gradient = ctx.createLinearGradient(0, 0, 0, 300);
-          gradient.addColorStop(0, "#090979");
-          gradient.addColorStop(1, "#020024");
-          return gradient;
-        },
-      },
-    ],
-  };
-
-  const options = {
-    plugins: {
-      legend: true,
-    },
+  export const options = {
     responsive: true,
     scales: {
       y: {
-        ticks: {
-          font: {
-            size: 17,
-            weight: "bold",
-          },
-        },
-        title: {
-          display: true,
-          text: "Sales",
-          padding: {
-            bottom: 10,
-          },
-          font: {
-            size: 30,
-            style: "italic",
-            family: "Arial",
-          },
-        },
-        min: 50,
-      },
-      x: {
-        ticks: {
-          font: {
-            size: 17,
-            weight: "bold",
-          },
-        },
-        title: {
-          display: true,
-          text: "Month",
-          padding: {
-            top: 10,
-          },
-          font: {
-            size: 30,
-            style: "italic",
-            family: "Arial",
-          },
-        },
-      },
+        beginAtZero: true,
+      }
     },
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Line Chart',
+      },
+      zoom:  {
+        
+        pan: {
+          enabled: true, 
+          mode: 'xy',
+        },
+        zoom: {
+          wheel: {
+            enabled: true,
+          },
+          pinch : {
+            enabled: true,
+          },
+          mode: 'xy',
+          onZoomComplete({chart}) {
+            // This update is needed to display up to date zoom level in the title.
+            // Without this, previous zoom level is displayed.
+            // The reason is: title uses the same beforeUpdate hook, and is evaluated before zoom.
+            chart.update('none');
+          }
+      
+        }
+      }
+    }
+
   };
+  
+  const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  
+  export const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Dataset 1',
+        data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: 'rgba(255, 99, 132, 0.5)',
+      }
+    ],
+  };
+
+export default function Maquina({params}) {
+    
     
     let {id} = params;
     return (
